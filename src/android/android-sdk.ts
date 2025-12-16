3035a9419df89402b69555403ac528fd64d59b5a
Chris Barber <chris@cb1inc.com>
2025-12-16 00:47:03 -0600

    Update deps, more work on the Android SDK

--- src/android/android-sdk.ts
+++ src/android/android-sdk.ts
@@ -4,7 +4,7 @@ import { createHash } from 'node:crypto';
 import { tailgate } from '../util/tailgate.js';
 import snooplogg from 'snooplogg';
 import { isDir } from '../util/is-dir.js';
-import { join } from 'node:path';
+import { join, relative } from 'node:path';
 import { readPropertiesFile } from './util/read-properties-file.js';
 import { isFile } from '../util/is-file.js';
 import { readdir } from 'node:fs/promises';
@@ -14,33 +14,71 @@ const { error, log } = snooplogg('android:sdk');
 const bat = process.platform === 'win32' ? '.bat' : '';
 const exe = process.platform === 'win32' ? '.exe' : '';
 
-interface AndroidSDKExecutables {
+type AndroidSDKExecutables = {
 	adb: string;
 	android: string;
 	emulator: string;
-}
+};
 
-export class AndroidSDK {
+type SystemImage = {
+	abi: string;
+	sdk: string;
+	skins: string[];
+	type: string;
+};
+
+type Platform = {
+	abis: Record<string, string[]>;
+	androidJar: string | null;
+	apiLevel: number;
+	codename: string | null;
+	defaultSkin: string | null;
+	minToolsRev: number | null;
+	name: string;
+	path: string;
+	revision: number | null;
+	sdk: string;
+	skins: string[];
+	version: string;
+};
+
+type Addon = {
+	abis: Record<string, string[]> | null;
+	androidJar: string | null;
+	apiLevel: number;
+	basedOn: { version: string; apiLevel: number } | null;
+	codename: string | null;
+	defaultSkin: string | null;
+	description: string | null;
+	minToolsRev: number | null;
+	name: string;
+	path: string;
+	revision: number | null;
+	sdk: string;
+	skins: string[] | null;
+	vendor: string;
+	version: string | null;
+};
+
+type AndroidSDKOptions = {
+	addons: Addon[];
 	buildTools: Record<string, string>;
 	executables: AndroidSDKExecutables;
 	path: string;
+	systemImages: Record<string, SystemImage>;
 	version: string;
+};
 
-	constructor({
-		buildTools,
-		executables,
-		path,
-		version
-	}: {
-		buildTools: Record<string, string>;
-		executables: AndroidSDKExecutables;
-		path: string;
-		version: string;
-	}) {
-		this.buildTools = buildTools;
-		this.executables = executables;
-		this.path = path;
-		this.version = version;
+export class AndroidSDK {
+	addons!: Addon[];
+	buildTools!: Record<string, string>;
+	executables!: AndroidSDKExecutables;
+	path!: string;
+	systemImages!: Record<string, SystemImage>;
+	version!: string;
+
+	constructor(options: AndroidSDKOptions) {
+		Object.assign(this, options);
 	}
 
 	static async load(path: string): Promise<AndroidSDK> {
@@ -66,178 +104,205 @@ export class AndroidSDK {
 			}
 		}
 
-		const systemImagesDir = join(path, 'system-images');
-
-		// /**
-		//  * Detect system images
-		//  */
-		// const systemImagesDir = path.join(dir, 'system-images');
-		// if (isDir(systemImagesDir)) {
-		// 	for (const platform of fs.readdirSync(systemImagesDir)) {
-		// 		const platformDir = path.join(systemImagesDir, platform);
-		// 		if (isDir(platformDir)) {
-		// 			for (const tag of fs.readdirSync(platformDir)) {
-		// 				const tagDir = path.join(platformDir, tag);
-		// 				if (isDir(tagDir)) {
-		// 					for (const abi of fs.readdirSync(tagDir)) {
-		// 						const abiDir = path.join(tagDir, abi);
-		// 						const props = readPropertiesFile(path.join(abiDir, 'source.properties'));
-		// 						if (props && props['AndroidVersion.ApiLevel'] && props['SystemImage.TagId'] && props['SystemImage.Abi']) {
-		// 							const imageDir = path.relative(systemImagesDir, abiDir).replace(/\\/g, '/');
-		// 							const skinsDir = path.join(abiDir, 'skins');
-
-		// 							this.systemImages[imageDir] = {
-		// 								abi: props['SystemImage.Abi'],
-		// 								sdk: `android-${props['AndroidVersion.CodeName'] || props['AndroidVersion.ApiLevel']}`,
-		// 								skins: isDir(skinsDir) ? fs.readdirSync(skinsDir).map(name => {
-		// 									return isFile(path.join(skinsDir, name, 'hardware.ini')) ? name : null;
-		// 								}).filter(x => x) : [],
-		// 								type: props['SystemImage.TagId']
-		// 							};
-		// 						}
-		// 					}
-		// 				}
-		// 			}
-		// 		}
-		// 	}
-		// }
-
-		// /**
-		//  * Detect platforms
-		//  */
-		// const platformsDir = path.join(dir, 'platforms');
-		// if (isDir(platformsDir)) {
-		// 	for (const name of fs.readdirSync(platformsDir)) {
-		// 		const dir = path.join(platformsDir, name);
-		// 		const sourceProps = readPropertiesFile(path.join(dir, 'source.properties'));
-		// 		const apiLevel = sourceProps ? ~~sourceProps['AndroidVersion.ApiLevel'] : null;
-		// 		if (!sourceProps || !apiLevel || !isFile(path.join(dir, 'build.prop'))) {
-		// 			continue;
-		// 		}
-
-		// 		// read in the sdk properties, if exists
-		// 		const sdkProps = readPropertiesFile(path.join(dir, 'sdk.properties'));
-
-		// 		// detect the available skins
-		// 		const skinsDir = path.join(dir, 'skins');
-		// 		const skins = isDir(skinsDir) ? fs.readdirSync(skinsDir).map(name => {
-		// 			return isFile(path.join(skinsDir, name, 'hardware.ini')) ? name : null;
-		// 		}).filter(x => x) : [];
-		// 		let defaultSkin = sdkProps && sdkProps['sdk.skin.default'];
-		// 		if (skins.indexOf(defaultSkin) === -1 && skins.indexOf(defaultSkin = 'WVGA800') === -1) {
-		// 			defaultSkin = skins[skins.length - 1] || null;
-		// 		}
-
-		// 		const apiName = sourceProps['AndroidVersion.CodeName'] || apiLevel;
-		// 		const sdk = `android-${apiName}`;
-		// 		let tmp;
-
-		// 		const abis = {};
-		// 		for (const image of Object.values(this.systemImages)) {
-		// 			if (image.sdk === sdk) {
-		// 				if (!abis[image.type]) {
-		// 					abis[image.type] = [];
-		// 				}
-		// 				if (!abis[image.type].includes(image.abi)) {
-		// 					abis[image.type].push(image.abi);
-		// 				}
-
-		// 				for (const skin of image.skins) {
-		// 					if (!skins.includes(skin)) {
-		// 						skins.push(skin);
-		// 					}
-		// 				}
-		// 			}
-		// 		}
-
-		// 		this.platforms.push({
-		// 			abis:        abis,
-		// 			aidl:        isFile(tmp = path.join(dir, 'framework.aidl')) ? tmp : null,
-		// 			androidJar:  isFile(tmp = path.join(dir, 'android.jar')) ? tmp : null,
-		// 			apiLevel:    apiLevel,
-		// 			codename:    sourceProps['AndroidVersion.CodeName'] || null,
-		// 			defaultSkin: defaultSkin,
-		// 			minToolsRev: +sourceProps['Platform.MinToolsRev'] || null,
-		// 			name:        `Android ${sourceProps['Platform.Version']}${sourceProps['AndroidVersion.CodeName'] ? ' (Preview)' : ''}`,
-		// 			path:        dir,
-		// 			revision:    +sourceProps['Layoutlib.Revision'] || null,
-		// 			sdk,
-		// 			skins:       skins,
-		// 			version:     sourceProps['Platform.Version']
-		// 		});
-		// 	}
-		// }
-
-		// /**
-		//  * Detect addons
-		//  */
-		// const addonsDir = path.join(dir, 'add-ons');
-		// if (isDir(addonsDir)) {
-		// 	for (const name of fs.readdirSync(addonsDir)) {
-		// 		const dir = path.join(addonsDir, name);
-		// 		const props = readPropertiesFile(path.join(dir, 'source.properties')) || readPropertiesFile(path.join(dir, 'manifest.ini'));
-		// 		if (!props) {
-		// 			continue;
-		// 		}
-
-		// 		const apiLevel = parseInt(props['AndroidVersion.ApiLevel'] || props.api);
-		// 		const vendorDisplay = props['Addon.VendorDisplay'] || props.vendor;
-		// 		const nameDisplay = props['Addon.NameDisplay'] || props.name;
-		// 		if (!apiLevel || isNaN(apiLevel) || !vendorDisplay || !nameDisplay) {
-		// 			continue;
-		// 		}
-
-		// 		let basedOn = null;
-		// 		for (const platform of this.platforms) {
-		// 			if (platform.codename === null && platform.apiLevel === apiLevel) {
-		// 				basedOn = platform;
-		// 				break;
-		// 			}
-		// 		}
-
-		// 		this.addons.push({
-		// 			abis:        basedOn && basedOn.abis || null,
-		// 			aidl:        basedOn && basedOn.aidl || null,
-		// 			androidJar:  basedOn && basedOn.androidJar || null,
-		// 			apiLevel:    apiLevel,
-		// 			basedOn:     basedOn ? { version: basedOn.version, apiLevel: basedOn.apiLevel } : null,
-		// 			codename:    props['AndroidVersion.CodeName'] || props.codename || null,
-		// 			defaultSkin: basedOn && basedOn.defaultSkin || null,
-		// 			description: props['Pkg.Desc'] || props.description || null,
-		// 			minToolsRev: basedOn && basedOn.minToolsRev || null,
-		// 			name:        nameDisplay,
-		// 			path:        dir,
-		// 			revision:    parseInt(props['Pkg.Revision'] || props.revision) || null,
-		// 			sdk:         `${vendorDisplay}:${nameDisplay}:${apiLevel}`,
-		// 			skins:       basedOn && basedOn.skins || null,
-		// 			vendor: 	 vendorDisplay,
-		// 			version:	 basedOn && basedOn.version || null
-		// 		});
-		// 	}
-		// }
-
-		// function sortFn(a, b) {
-		// 	if (a.codename === null) {
-		// 		if (b.codename !== null && a.apiLevel === b.apiLevel) {
-		// 			// sort GA releases before preview releases
-		// 			return -1;
-		// 		}
-		// 	} else if (a.apiLevel === b.apiLevel) {
-		// 		return b.codename === null ? 1 : a.codename.localeCompare(b.codename);
-		// 	}
-
-		// 	return a.apiLevel - b.apiLevel;
-		// }
-
-		// this.platforms.sort(sortFn);
-		// this.addons.sort(sortFn);
+		const systemImages = await AndroidSDK.detectSystemImages(path);
+		const platforms = await AndroidSDK.detectPlatforms(path, systemImages);
+		const addons = await AndroidSDK.detectAddons(path, platforms);
 
 		return new AndroidSDK({
+			addons,
 			buildTools,
 			executables,
 			path,
+			systemImages,
 			version: ''
 		});
+	}
+
+	static async detectSystemImages(path: string): Promise<Record<string, SystemImage>> {
+		const systemImages: Record<string, SystemImage> = {};
+		const systemImagesDir = join(path, 'system-images');
+
+		if (!isDir(systemImagesDir)) {
+			return systemImages;
+		}
+
+		for (const platform of await readdir(systemImagesDir)) {
+			const platformDir = join(systemImagesDir, platform);
+			if (!isDir(platformDir)) {
+				continue;
+			}
+
+			for (const tag of await readdir(platformDir)) {
+				const tagDir = join(platformDir, tag);
+				if (!isDir(tagDir)) {
+					continue;
+				}
+
+				for (const abi of await readdir(tagDir)) {
+					const abiDir = join(tagDir, abi);
+					if (!isDir(abiDir)) {
+						continue;
+					}
+
+					const props = await readPropertiesFile(join(abiDir, 'source.properties'));
+					if (props?.['AndroidVersion.ApiLevel'] && props['SystemImage.TagId'] && props['SystemImage.Abi']) {
+						const imageDir = relative(systemImagesDir, abiDir).replace(/\\/g, '/');
+						const skinsDir = join(abiDir, 'skins');
+						const skins: string[] = [];
+						if (isDir(skinsDir)) {
+							for (const name of await readdir(skinsDir)) {
+								if (isFile(join(skinsDir, name, 'hardware.ini'))) {
+									skins.push(name);
+								}
+							}
+						}
+						systemImages[imageDir] = {
+							abi: props['SystemImage.Abi'],
+							sdk: `android-${props['AndroidVersion.CodeName'] || props['AndroidVersion.ApiLevel']}`,
+							skins,
+							type: props['SystemImage.TagId']
+						};
+					}
+				}
+			}
+		}
+
+		return systemImages;
+	}
+
+	static async detectPlatforms(path: string, systemImages: Record<string, SystemImage>): Promise<Platform[]> {
+		const platforms: Platform[] = [];
+		const platformsDir = join(path, 'platforms');
+		if (!isDir(platformsDir)) {
+			return platforms;
+		}
+
+		for (const name of await readdir(platformsDir)) {
+			const dir = join(platformsDir, name);
+			if (!isFile(join(dir, 'build.prop'))) {
+				continue;
+			}
+
+			const sourceProps = await readPropertiesFile(join(dir, 'source.properties'));
+			if (!sourceProps) {
+				continue;
+			}
+
+			const apiLevel = sourceProps['AndroidVersion.ApiLevel']
+				? Number.parseInt(sourceProps['AndroidVersion.ApiLevel'])
+				: null;
+			if (!apiLevel) {
+				continue;
+			}
+
+			// read in the sdk properties, if exists
+			const sdkProps = await readPropertiesFile(join(dir, 'sdk.properties'));
+
+			// detect the available skins
+			const skinsDir = join(dir, 'skins');
+			const skins: string[] = [];
+			if (isDir(skinsDir)) {
+				for (const name of await readdir(skinsDir)) {
+					if (isFile(join(skinsDir, name, 'hardware.ini'))) {
+						skins.push(name);
+					}
+				}
+			}
+			let defaultSkin: string | null = sdkProps?.['sdk.skin.default'] ?? 'WVGA800';
+			if (defaultSkin && skins.includes(defaultSkin) && !skins.includes('WVGA800')) {
+				defaultSkin = skins[skins.length - 1] || null;
+			}
+
+			const apiName = sourceProps?.['AndroidVersion.CodeName'] || apiLevel;
+			const sdk = `android-${apiName}`;
+
+			const abis: Record<string, string[]> = {};
+			for (const image of Object.values(systemImages)) {
+				if (image.sdk === sdk) {
+					if (!abis[image.type]) {
+						abis[image.type] = [];
+					}
+					if (!abis[image.type].includes(image.abi)) {
+						abis[image.type].push(image.abi);
+					}
+					for (const skin of image.skins) {
+						if (!skins.includes(skin)) {
+							skins.push(skin);
+						}
+					}
+				}
+			}
+
+			const androidJarFile = join(dir, 'android.jar');
+
+			platforms.push({
+				abis:        abis,
+				androidJar:  isFile(androidJarFile) ? androidJarFile : null,
+				apiLevel,
+				codename:    sourceProps['AndroidVersion.CodeName'] || null,
+				defaultSkin,
+				minToolsRev: sourceProps['Platform.MinToolsRev'] ? Number.parseInt(sourceProps['Platform.MinToolsRev']) : null,
+				name:        `Android ${sourceProps['Platform.Version']}${sourceProps['AndroidVersion.CodeName'] ? ' (Preview)' : ''}`,
+				path:        dir,
+				revision:    sourceProps['Layoutlib.Revision'] ? Number.parseInt(sourceProps['Layoutlib.Revision']) : null,
+				sdk,
+				skins,
+				version:     sourceProps['Platform.Version']
+			});
+		}
+
+		return platforms.sort(sortPackages);
+	}
+
+	static async detectAddons(path: string, platforms: Platform[]): Promise<Addon[]> {
+		const addons: Addon[] = [];
+		const addonsDir = join(path, 'add-ons');
+		if (!isDir(addonsDir)) {
+			return addons;
+		}
+
+		for (const name of await readdir(addonsDir)) {
+			const dir = join(addonsDir, name);
+			const props = await readPropertiesFile(join(dir, 'source.properties')) || await readPropertiesFile(join(dir, 'manifest.ini'));
+			if (!props) {
+				continue;
+			}
+
+			const apiLevel = Number.parseInt(props['AndroidVersion.ApiLevel'] || props.api);
+			const vendorDisplay = props['Addon.VendorDisplay'] || props.vendor;
+			const nameDisplay = props['Addon.NameDisplay'] || props.name;
+			if (!apiLevel || isNaN(apiLevel) || !vendorDisplay || !nameDisplay) {
+				continue;
+			}
+
+			let basedOn: Platform | null = null;
+			for (const platform of platforms) {
+				if (platform.codename === null && platform.apiLevel === apiLevel) {
+					basedOn = platform;
+					break;
+				}
+			}
+
+			addons.push({
+				abis:        basedOn?.abis || null,
+				androidJar:  basedOn?.androidJar || null,
+				apiLevel:    apiLevel,
+				basedOn:     basedOn ? { version: basedOn.version, apiLevel: basedOn.apiLevel } : null,
+				codename:    props['AndroidVersion.CodeName'] || props.codename || null,
+				defaultSkin: basedOn && basedOn.defaultSkin || null,
+				description: props['Pkg.Desc'] || props.description || null,
+				minToolsRev: basedOn && basedOn.minToolsRev || null,
+				name:        nameDisplay,
+				path:        dir,
+				revision:    Number.parseInt(props['Pkg.Revision'] || props.revision) || null,
+				sdk:         `${vendorDisplay}:${nameDisplay}:${apiLevel}`,
+				skins:       basedOn && basedOn.skins || null,
+				vendor: 	 vendorDisplay,
+				version:	 basedOn && basedOn.version || null
+			});
+		}
+
+		return addons.sort(sortPackages);
 	}
 }
 
@@ -261,17 +326,6 @@ export async function detect(options: {
 	});
 }
 
-function findExecutables<T extends Record<string, string>>(dir: string, exes: T): Record<keyof T, string> {
-	const executables: Record<string, string> = {};
-	for (const [ name, exe ] of Object.entries(exes)) {
-		const p = join(dir, exe);
-		if (isFile(p)) {
-			executables[name] = p;
-		}
-	}
-	return executables as Record<keyof T, string>;
-}
-
 async function getSearchPaths(options: { searchPaths?: string[] }) {
 	const paths: string[] = [];
 	if (Array.isArray(options.searchPaths)) {
@@ -290,4 +344,22 @@ async function getSearchPaths(options: { searchPaths?: string[] }) {
 	}
 
 	return Array.from(searchPaths);
+}
+
+type SortablePackage = {
+	apiLevel: number;
+	codename: string | null;
+}
+
+function sortPackages<T extends SortablePackage>(a: T, b: T) {
+	if (a.codename === null) {
+		if (b.codename !== null && a.apiLevel === b.apiLevel) {
+			// sort GA releases before preview releases
+			return -1;
+		}
+	} else if (a.apiLevel === b.apiLevel) {
+		return b.codename === null ? 1 : a.codename.localeCompare(b.codename);
+	}
+
+	return a.apiLevel - b.apiLevel;
 }
